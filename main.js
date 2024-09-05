e73be2078174a10ae5999be77cdf894938e370f1
Ricardo <santandergualdron@gmail.com>
2024-09-05 14:34:49 +0200

    Beautiful things are happening

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 // Constants
-const FOV = 75;
+const FOV = 75;  // Field of view
 const NEAR_PLANE = 10;
-const FAR_PLANE = 1000;
+const FAR_PLANE = 800;
 const Z_POSITION = -150;
-const Z_OFFSET = 1;
-const ROTATION_SPEED = 0.002;
+const Z_OFFSET = 2;
+const ROTATION_SPEED = 0.005;
 const ANIMATION_SPEED = 0.99;
 const CONTAINER_SIZE = { width: 400, height: 240 };
 const SCALE_FACTOR = 4;
 const Y_OFFSET = 170;
 const HALF_PI = Math.PI / 2;
-const MAX_PARTICLES = 2000; // Maximum number of particles to keep in the scene
-const MAX_CONTAINER_SCALE = 50; // Maximum scale for the container
+const MAX_PARTICLES = 3000; // Maximum number of particles to keep in the scene
 const allIds = [];
 let interestingIdsSet; // Changed to a Set for faster lookups
+const MAX_PARTICLES_CURRENT = 1200;
+const MAX_CONTAINERS = 100;
+let addTrace = true;
+let end = false;
+const lineOpacity = 0.5;
 
 // Three.js setup
 const scene = new THREE.Scene();
@@ -23,24 +27,121 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Fog setup
-const farFog = Math.abs(Z_POSITION * 3);
+const farFog = Math.abs(Z_POSITION * 2);
 const nearFog = Math.abs(Z_POSITION / 3);
-scene.fog = new THREE.Fog(0xff00ff, nearFog, farFog);
+scene.fog = new THREE.Fog(0x1a000a, nearFog, farFog);
 
 // Particles
 const particleData = [];
-let currentTimeStep = 1;
+const particles = [];
+
+
+// create particles, add them to the scene, ad set them to invisible
+// The particles can be rendered as a box
+function createParticlePool(maxParticles) {
+    for (let i = 0; i < maxParticles; i++) {
+        const particleMesh = new THREE.Line(particleGeometry, new THREE.LineBasicMaterial({color: 0xffffff}));
+        particleMesh.visible = false;
+        scene.add(particleMesh);
+        particles.push(particleMesh);
+    }
+}
+// Create shared geometries
+const particleGeometry = new THREE.BoxGeometry(1, 0.001, 0);
+const sharedEdges = new THREE.EdgesGeometry(particleGeometry);
+
+createParticlePool(MAX_PARTICLES_CURRENT);
+
+function updateParticles(timeStep) {
+    const currentLayer = particleData[Math.floor(timeStep)] || [];
+    let particleIndex = 0;
+    let somethingWasUpdated = false;
+   
+    currentLayer.forEach(data => {
+                if (interestingIdsSet.has(data.ID) && particleIndex < particles.length) {
+                    const particle = particles[particleIndex];
+                    const x = data.x * SCALE_FACTOR;
+                    const y = (data.y - Y_OFFSET) * SCALE_FACTOR;
+                    const z = -timeStep * Z_OFFSET - 0.5;
+                    const length = (data.length + 1) * SCALE_FACTOR;
+                    const angle = data.angle * Math.PI + HALF_PI;
+        
+                    particle.scale.set(length, 1, 1);
+                    particle.position.set(x, y, z);
+                    particle.rotation.z = angle;
+                    particle.material = materialGreen;
+                    particle.visible = true;
+                    
+                    particleIndex++;
+                    somethingWasUpdated = true;
+
+                }
+                
+            });
+
+      
+  
+    // Hide unused particles
+    for (let i = particleIndex; i < particles.length; i++) {
+        particles[i].visible = false;
+    }
+
+
+    if (!somethingWasUpdated && currentTimeStep > 1) {
+        end = true;
+    }
+}
+
+    
+
+
+
+
+
+let currentTimeStep = 0;
 let numberOfTimeSteps = null;
 const addedParticles = []; // Array to store added particles
+const addedPoints = []; // Array to store added points
+const addedContainers = []; // Array to store added container
 
 // Create container
-const containerGeometry = new THREE.BoxGeometry(CONTAINER_SIZE.width, CONTAINER_SIZE.height, 20);
-const containerEdges = new THREE.EdgesGeometry(containerGeometry);
-const container = new THREE.LineSegments(containerEdges, new THREE.LineBasicMaterial({color: 0xffffff}));
-scene.add(container);
-const material = new THREE.LineBasicMaterial({ color: 0xffffff, 
+let container;
+function createContainer(timeStep) {
+    const thickness =  0;
+    const containerGeometry = new THREE.BoxGeometry(CONTAINER_SIZE.width, CONTAINER_SIZE.height, thickness);
+    const containerEdges = new THREE.EdgesGeometry(containerGeometry);
+    container = new THREE.LineSegments(containerEdges, new THREE.LineBasicMaterial({color: 0xffffff}));
+    container.position.z = -(Z_OFFSET * timeStep) ;
+    scene.add(container);
+    addedContainers.push(container);
+}
+
+createContainer(currentTimeStep);
+
+const staticContainer = new THREE.BoxGeometry(CONTAINER_SIZE.width, CONTAINER_SIZE.height, 0);
+const staticContainerEdges = new THREE.EdgesGeometry(staticContainer);
+const staticContainerLine = new THREE.LineSegments(staticContainerEdges, new THREE.LineBasicMaterial({color: 0xffffff}));
+staticContainerLine.position.z = 0;
+scene.add(staticContainerLine);
+
+const material = new THREE.LineBasicMaterial({ 
+    color: 0xffffff, 
     transparent: true, 
-    opacity: 0.5,
+    opacity: lineOpacity,
+    linewidth: 4 // Increased line width (note: this may not be supported in all WebGL renderers)
+});
+
+const materialGreen = new THREE.LineBasicMaterial({ 
+    color: 0x00ffff, 
+    transparent: true, 
+    opacity: lineOpacity,
+    linewidth: 10// Increased line width (note: this may not be supported in all WebGL renderers)
+});
+const materialPoints = new THREE.PointsMaterial({color: 0xff00ff, 
+    size: 1.5,
+    sizeAttenuation: true,
+    transparent: true,
+    opacity: lineOpacity
 });
 let angle = 0;
 
@@ -77,7 +178,7 @@ function generateInterestingIds() {
     const interestingIds = [];
     const initialId = BigInt(randomInt);
     console.log(initialId);
-    const generations = 9; // Number of generations to add
+    const generations = 16; // Number of generations to add
 
     // Add the initial ID
     interestingIds.push(initialId);
@@ -95,52 +196,95 @@ function generateInterestingIds() {
 
     // Convert interesting IDs to a Set for faster lookups
     interestingIdsSet = new Set(interestingIds.map(id => id.toString()));
+    // print length of interestingIds
+    console.log(interestingIds.length);
 }
 
 // add an event listener to restart the animation when the user clicks the s key
 document.addEventListener('keydown', (event) => {
     if (event.key === 's') {
-        currentTimeStep = 1;
-        addedParticles.forEach(particle => {
-            scene.remove(particle);
-            particle.geometry.dispose();
-            particle.material.dispose();
-        });
-        addedParticles.length = 0;
+        restart()
+
         randomInt = Math.floor(Math.random() * (1000)) + 1000;
 
         generateInterestingIds();
+        createContainer(currentTimeStep);
     }
+    else if (event.key === 't') {
+        addTrace = !addTrace;
+    }
+    
 });
 
 function animate() {
     requestAnimationFrame(animate);
 
-    if (particleData.length > 0) {
+    if (particleData.length > 0 &&  end === false) {
         currentTimeStep = (currentTimeStep + ANIMATION_SPEED) % numberOfTimeSteps;
 
-        if(addedParticles.length > MAX_PARTICLES*1.1) {
+  
+
+        if(addedParticles.length > MAX_PARTICLES) {
             removeOldParticles(); // Remove old particles before adding new ones
         }
-
+        if(addedContainers.length > MAX_CONTAINERS) {
+            removeOldContainers(); // Remove old containers before adding new ones
+        }
+        if (addTrace) {
         addParticles(currentTimeStep);
+        }
+        if (Math.floor(currentTimeStep) % 10 === 0) {
+            createContainer(currentTimeStep);
+        }
+
+        updateParticles(currentTimeStep);
+
+    
         
-        container.position.z = -Z_OFFSET * currentTimeStep;
     }
 
     angle += ROTATION_SPEED * ANIMATION_SPEED;
     const cameraX = 120 * Math.cos(angle);
     const cameraY = 120 * Math.sin(angle);
-    const cameraZ = -Z_OFFSET * currentTimeStep + Z_POSITION;
+    let cameraZ = -Z_OFFSET * currentTimeStep + Z_POSITION;
+    if (end === false){
+        cameraZ = -Z_OFFSET * currentTimeStep+ Z_POSITION;
+    } else {
+        cameraZ += 5;
+    }
     camera.position.set(cameraX, cameraY, cameraZ);
-    camera.lookAt(0, 0, -Z_OFFSET * currentTimeStep);    
-
+    camera.lookAt(cameraX*0.5, cameraY*0.5, -Z_OFFSET * currentTimeStep);    
+    staticContainerLine.position.z = -Z_OFFSET * currentTimeStep;
     renderer.render(scene, camera);
 }
 
+function restart() {
+    currentTimeStep = 1;
+    addedParticles.forEach(particle => {
+        scene.remove(particle);
+        particle.geometry.dispose();
+        particle.material.dispose();
+    });
+    addedParticles.length = 0;
+    addedPoints.forEach(point => {
+        scene.remove(point);
+        point.geometry.dispose();
+        point.material.dispose();
+    });
+    addedPoints.length = 0;
+    addedContainers.forEach(container => {
+        scene.remove(container);
+        container.geometry.dispose();
+        container.material.dispose();
+    });
+    addedContainers.length = 0;
+
+    createContainer(currentTimeStep);
+
+}
+
+
 function addParticles(timeStep) {
-
-
     const z = -timeStep * Z_OFFSET;
     const layer = particleData[Math.floor(timeStep)] || [];
 
@@ -148,10 +292,10 @@ function addParticles(timeStep) {
         if (interestingIdsSet.has(data.ID)) {
             const x = data.x * SCALE_FACTOR;
             const y = (data.y - Y_OFFSET) * SCALE_FACTOR;
-            const length = data.length * SCALE_FACTOR;
+            const length = (data.length+1) * SCALE_FACTOR;
             const angle = data.angle * Math.PI + HALF_PI;
 
-            const startX = x - length * Math.cos(angle) / 2;
+            const startX = x - length * Math.cos(angle) / 2
             const startY = y - length * Math.sin(angle) / 2;
             const endX = x + length * Math.cos(angle) / 2;
             const endY = y + length * Math.sin(angle) / 2;
@@ -160,8 +304,18 @@ function addParticles(timeStep) {
                 new THREE.Vector3(endX, endY, z)
             ]);
 
+            //Let's place a point at the start and end of the line
+             const points = [];
+                points.push(new THREE.Vector3(startX, startY, z));
+                points.push(new THREE.Vector3(endX, endY, z));
+                const geometryPoints = new THREE.BufferGeometry().setFromPoints(points);
+
+                const pointsObject = new THREE.Points(geometryPoints, materialPoints);
+                scene.add(pointsObject);
+                addedPoints.push(pointsObject);
+
+
             const line = new THREE.Line(geometry, material);
-            line.lineWidth = 3;
             scene.add(line);
             addedParticles.push(line); // Store the added particle
         }
@@ -174,11 +328,25 @@ function removeOldParticles() {
         scene.remove(oldestParticle); // Remove the particle from the scene
         oldestParticle.geometry.dispose(); // Dispose of the geometry
         oldestParticle.material.dispose(); // Dispose of the material
-        
-    
+    }
+
+    while (addedPoints.length > MAX_PARTICLES) {
+        const oldestPoint = addedPoints.shift(); // Remove the oldest point from the array
+        scene.remove(oldestPoint); // Remove the point from the scene
+        oldestPoint.geometry.dispose(); // Dispose of the geometry
+        oldestPoint.material.dispose(); // Dispose of the material
+    }
+
+
+}
+
+function removeOldContainers() {
+    while (addedContainers.length > MAX_CONTAINERS) {
+        const oldestContainer = addedContainers.shift(); // Remove the oldest container from the array
+        scene.remove(oldestContainer); // Remove the container from the scene
+        oldestContainer.geometry.dispose(); // Dispose of the geometry
+        oldestContainer.material.dispose(); // Dispose of the material
     }
 }
 
 animate();
-
-
